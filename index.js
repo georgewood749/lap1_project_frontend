7710e2e7c49cb02a130abb8dd1ff04401314887e
georgewood749 <georgewood749@gmail.com>
2022-10-03 17:29:09 +0100

    reorganised files and giphy api

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const fetchAsync = async (searchTerm) => {
     const gifData = await rawData.json()
     console.log(gifData);
 
-    const gif1url = gifData.data[0].bitly_gif_url
+    const gif1url = gifData.data[0].url
     const gif2url = gifData.data[1].embed_url
     const gif3url = gifData.data[2].embed_url
     const gif4url = gifData.data[3].embed_url
@@ -44,21 +44,6 @@ const fetchAsync = async (searchTerm) => {
     gif4.src = gif4url
 }
 
-// const fetchAsyncTrending = async () => {
-//     const rawData = await fetch(`https://api.giphy.com/v1/gifs/trending`)
-//     const gifData = await rawData.json()
-//     console.log(gifData);
-
-//     const gif1url = gifData.data[0].bitly_gif_url
-//     const gif2url = gifData.data[1].embed_url
-//     const gif3url = gifData.data[2].embed_url
-//     const gif4url = gifData.data[3].embed_url
-
-//     gif1.src = gif1url
-//     gif2.src = gif2url
-//     gif3.src = gif3url
-//     gif4.src = gif4url
-// }
 
 const searchGIF = document.getElementById('gifSearchTerm')
 const gifButton = document.getElementById('addGif')
