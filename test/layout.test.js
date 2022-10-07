c1bd5b0f219fcb71aa720e5e51dfbcae7ea60922
Ami <ami1996_tm@live.co.uk>
2022-10-07 13:08:12 +0100

    Added layout tests for textarea and counter

--- test/layout.test.js
+++ test/layout.test.js
@@ -1,14 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+const fs = require('fs')
+const path = require('path')
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8')
 
 describe('Testing index.html', () => {
     beforeEach(() => {
-        document.documentElement.innerHTML = html.toString();
+        document.documentElement.innerHTML = html.toString()
     })
 
     test('it has a title', () => {
-        const title = document.querySelector('title');
-        expect(title).toBeTruthy();
+        const title = document.querySelector('title')
+        expect(title).toBeTruthy()
+    })
+
+    test('it has a textarea element with max length 180', () => {
+        const textInput = document.querySelector('textarea')
+        expect(parseInt(textInput.maxLength)).toBe(170)
+    })
+
+    test('it has a character counter with initial value of 180', () => {
+        const counter = document.getElementById('counter')
+        expect(parseInt(counter.value)).toBe(180)
     })
 })
